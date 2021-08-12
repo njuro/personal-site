ad64f04ee56875598a281ca62f43e9c283f045ba
njuro <juraj.noge@gmail.com>
2021-08-12 16:47:43 +0200

    Socials in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,33 @@
 import React from "react";
-import { Icon, Segment, SegmentProps } from "semantic-ui-react";
+import {
+  Divider,
+  Icon,
+  Segment,
+  SegmentProps,
+  SemanticICONS,
+} from "semantic-ui-react";
 import styled from "styled-components";
+import {
+  EMAIL_ADDRESS,
+  GITHUB_URL,
+  GOODREADS_URL,
+  LINKEDIN_URL,
+} from "../mappings";
+
+interface ContactIconProps {
+  name: SemanticICONS;
+  url: string;
+}
+function ContactIcon({ name, url }: ContactIconProps) {
+  return (
+    <Icon
+      name={name}
+      link
+      size="large"
+      onClick={() => window.open(url, "_blank", "noreferrer")}
+    />
+  );
+}
 
 const FooterContainer: React.FC<SegmentProps> = styled(Segment)`
   height: 80px;
@@ -21,20 +48,53 @@ const FooterContainer: React.FC<SegmentProps> = styled(Segment)`
   }
 `;
 function Footer() {
+  const contacts: ContactIconProps[] = [
+    {
+      name: "github",
+      url: GITHUB_URL,
+    },
+    {
+      name: "linkedin",
+      url: LINKEDIN_URL,
+    },
+    {
+      name: "goodreads",
+      url: GOODREADS_URL,
+    },
+    {
+      name: "mail",
+      url: `mailto:${EMAIL_ADDRESS}`,
+    },
+  ];
+
   return (
-    <FooterContainer vertical as="footer">
-      Created with&nbsp;
-      <Icon name="heart" />
-      and&nbsp;
-      <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-        React
-      </a>
-      , UI components by{" "}
-      <a href="https://react.semantic-ui.com/" target="_blank" rel="noreferrer">
-        Semantic UI
-      </a>
-      . &copy; 2021 Juraj Noge
-    </FooterContainer>
+    <>
+      <FooterContainer vertical as="footer">
+        Created with&nbsp;
+        <Icon name="heart" />
+        and&nbsp;
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          React
+        </a>
+        , UI components by{" "}
+        <a
+          href="https://react.semantic-ui.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Semantic UI
+        </a>
+        . &copy; 2021 Juraj Noge
+        <br />
+        {contacts.map((contact) => (
+          <ContactIcon
+            name={contact.name}
+            url={contact.url}
+            key={contact.name}
+          />
+        ))}
+      </FooterContainer>
+    </>
   );
 }
 
