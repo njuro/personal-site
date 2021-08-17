85ecad0a9963b99c81318764bddc2f9c0e6b1f86
njuro <juraj.noge@gmail.com>
2021-08-17 11:07:38 +0200

    Style 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
+import { Header } from "semantic-ui-react";
 import useNavigation from "../useNavigation";
-import { NOT_FOUND_URL } from "../mappings";
+import { HOME_URL, NOT_FOUND_URL } from "../mappings";
 
 function NotFound() {
   useNavigation(undefined);
@@ -9,7 +10,21 @@ function NotFound() {
 
   useEffect(() => history.push(NOT_FOUND_URL), [history]);
 
-  return <div>404</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Header as="h1" style={{ textAlign: "center" }}>
+        404 - Not Found
+      </Header>
+      <p style={{ fontSize: "18px", marginTop: "30px" }}>
+        Unfortunately I don&apos;t have this page set up (yet). May I offer you
+        a link to{" "}
+        <Link to={HOME_URL} className="content-link">
+          Home
+        </Link>{" "}
+        instead?
+      </p>
+    </div>
+  );
 }
 
 export default NotFound;
